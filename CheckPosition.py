64041d88a09444debf75437c53dbdf7718650da0
Lawson Fuller <llfuller@users.noreply.github.com>
2019-11-20 22:25:32 -0800

    Time Series Position and Velocity Operational
    Time series of position and velocity are generated by running SimulateCollisions.py, and their behavior can be graphed with CheckPosition.py.

--- CheckPosition.py
+++ CheckPosition.py
@@ -1,6 +1,35 @@
 from SimulateCollisions import *
 
-print("Particle 2 x_o: " + str(x2a_Arr[0]))
+#Same:
+# print("Particle 1 t=0 x_a: " + str(x1a_Arr[0]))
+# print("Particle 2 t=0 x_a: " + str(x2a_Arr[0]))
+# print("InitialPosition1a: " + str(InitialPosition1a[0][0]))
+# print("InitialPosition2a: " + str(InitialPosition2a[0][0]))
+# print("InitialPosition1n length: "+str(sp.sqrt(sp.power(InitialPosition1n[0][0],2)+sp.power(InitialPosition1n[0][1],2))))
+# print("v_cm: "+str(vcm))
+
 print("Particle 1 time to reach x=0: " + str(sp.fabs(sp.divide(x1a_Arr[0],v1ax_Arr[0]))))
 print("Particle 2 time to reach x=0: " + str(sp.fabs(sp.divide(x2a_Arr[0],v2ax_Arr[0]))))
-print()
+print("Particle 1 initial position: " + str(Position1L_t[:,0,0]) + " at v_x="+str(v1L[0][0]))
+print("Particle 2 initial position: " + str(Position2L_t[:,0,0]) + " at v_x="+str(v2L[0][0]))
+print("Angle of rotation: " + str(sp.divide(phi[0],sp.pi)) +" PI radians")
+
+plt.figure()
+plt.plot(Position1L_t[0,0,:], Position1L_t[1,0,:], label="Particle 1")
+plt.plot(Position2L_t[0,0,:], Position2L_t[1,0,:], label="Particle 2")
+plt.title("Collision")
+# plt.plot(Position1L_t_pre[0,0,:], Position1L_t_pre[1,0,:], label="1")
+# plt.plot(Position2L_t_pre[0,0,:], Position2L_t_pre[1,0,:], label="2")
+#
+# plt.plot(Position1L_t_post[0,0,:], Position1L_t_post[1,0,:], label="1")
+# plt.plot(Position2L_t_post[0,0,:], Position2L_t_post[1,0,:], label="2")
+plt.legend()
+
+plt.figure()
+print(sp.shape(t))
+print(sp.shape(Velocity1L_t))
+plt.plot(t[:,0], Velocity1L_t[0,0,:], label="Particle 1")
+plt.plot(t[:,0], Velocity2L_t[0,0,:], label="Particle 2")
+plt.title("V_x")
+plt.legend()
+plt.show()
--- SimulateCollisions.py
+++ SimulateCollisions.py
@@ -1,8 +1,10 @@
 import scipy as sp
 from scipy.stats import uniform as uni
+import time
+start_time = time.time()
 import matplotlib.pyplot as plt
 
-numTrials = 10
+numTrials = 6000
 
 # Notation:
 # Letter "a" in variable name means quantity is in aligned (mass 1 on +x axis) CM frame
@@ -32,12 +34,17 @@ def conservation(a_i, a_f):
     # print("a_f: " +str(a_f[0]))
     # print("error: " +str(sp.fabs(sp.divide((a_i - a_f),a_i))[0]))
     return str( False not in (sp.isclose(a_i,a_f)))
+def position(v_arr,t,pos_init):
+    # v_arr is dimension (numTrials, 2)
+    return None
 
 #===========================================
 #
 # CM Aligned Frame
 #
 #===========================================
+print("Beginning Aligned Center of Mass Phase: ")
+print(str(time.time() - start_time)+" seconds")
 
 # min and max of sampled values
 m_min = 0.001
@@ -46,8 +53,8 @@ x_min = 10
 x_max = 100
 v1ax_min = 0.001
 v1ax_max = 10
-vcm_min = -50
-vcm_max = 50
+vcm_min = -10
+vcm_max = 10
 
 # Random physical initial states for all trials with dimension (numTrials)
 m1_Arr = uni.rvs(m_min, m_max, size=numTrials)
@@ -58,7 +65,8 @@ vcm_x_Arr = uni.rvs(vcm_min, vcm_max, size=numTrials)
 vcm_y_Arr = uni.rvs(vcm_min, vcm_max, size=numTrials)
 
 # Always zero quantities with dimension (numTrials)
-y1a = sp.zeros(numTrials)
+y1a_Arr = sp.zeros(numTrials)
+y2a_Arr = sp.zeros(numTrials)
 v1ay_Arr = sp.zeros(numTrials)
 v2ay_Arr = sp.zeros(numTrials)
 
@@ -73,6 +81,8 @@ x2a_Arr = sp.multiply(-1,sp.multiply(v2ax_Arr,t_col))
 # Full aligned vectors with dimensions (2, numTrials)
 v1a = sp.array([v1ax_Arr,v1ay_Arr])
 v2a = sp.array([v2ax_Arr,v2ay_Arr])
+InitialPosition1a = sp.array([x1a_Arr,y1a_Arr])
+InitialPosition2a = sp.array([x2a_Arr,y2a_Arr])
 
 # Initial energy in CM frame
 E_i = energy(m1_Arr,m2_Arr,sp.transpose(v1a),sp.transpose(v2a))
@@ -97,28 +107,36 @@ p_y_f = y_momentum(m1_Arr,m2_Arr,sp.transpose(v1a_f),sp.transpose(v2a_f))
 # CM Unaligned Frame
 #
 #===========================================
+print("Beginning Rotation Phase")
+print(str(time.time() - start_time)+" seconds")
 
 # Rotation angles:
-phi = sp.multiply(sp.random.uniform(0,1,size=numTrials),2*sp.pi)
+phi = sp.multiply(sp.random.uniform(0,1,size=numTrials),2*sp.pi) #remove the zero
 # Rotation matrix with dimensions (2 , 2 , numTrials)
-R = sp.array([[sp.cos(phi), -sp.sin(phi)],[sp.sin(phi), sp.cos(phi)]])
+R = sp.array([[sp.cos(phi), sp.sin(phi)],[-sp.sin(phi), sp.cos(phi)]])
 
 v1n = sp.empty(shape=(numTrials, 2))
 v2n = sp.empty(shape=(numTrials, 2))
 v1n_f = sp.empty(shape=(numTrials, 2))
 v2n_f = sp.empty(shape=(numTrials, 2))
-# Rotated initially velocity vector of particles 1 and 2 with dimensions (numTrials, 2, 2)
+InitialPosition1n = sp.empty(shape=(numTrials, 2))
+InitialPosition2n = sp.empty(shape=(numTrials, 2))
+# Rotating initial vectors of particles 1 and 2 with dimensions (numTrials, 2)
 for trial in range(numTrials):
     v1n[trial] = sp.matmul(R[:,:,trial], v1a[:,trial])
     v2n[trial] = sp.matmul(R[:,:,trial], v2a[:,trial])
     v1n_f[trial] = sp.matmul(R[:,:,trial], v1a_f[:,trial])
     v2n_f[trial] = sp.matmul(R[:,:,trial], v2a_f[:,trial])
+    InitialPosition1n[trial] = sp.matmul(R[:,:,trial], InitialPosition1a[:,trial])
+    InitialPosition2n[trial] = sp.matmul(R[:,:,trial], InitialPosition2a[:,trial])
 
 #===========================================
 #
 # Lab Frame Velocities
 #
 #===========================================
+print("Beginning Lab Phase: ")
+print(str(time.time() - start_time)+" seconds")
 
 # Full center of mass velocity in lab frame. Dimension (numTrials , 2)
 vcm = sp.transpose(sp.array([vcm_x_Arr, vcm_y_Arr]))
@@ -128,6 +146,10 @@ v1L = v1n + vcm
 v2L = v2n + vcm
 v1L_f = v1n_f + vcm
 v2L_f = v2n_f + vcm
+
+# Initial positions in lab frame:
+InitialPosition1L = InitialPosition1n
+InitialPosition2L = InitialPosition2n
 
 #===========================================
 #
@@ -141,13 +163,43 @@ p_y_i = y_momentum(m1_Arr,m2_Arr,v1L,v2L)
 p_x_f = x_momentum(m1_Arr,m2_Arr,v1L_f,v2L_f)
 p_y_f = y_momentum(m1_Arr,m2_Arr,v1L_f,v2L_f)
 
+#===========================================
+#
+# Lab Frame Collision Time Series
+#
+#===========================================
+print("Beginning Path Phase: ")
+print(str(time.time() - start_time)+" seconds")
 
-# plt.figure()
-# plt.scatter(sp.linspace(m_min,m_max,numTrials),m1_Arr)
-# plt.show()
+timeStepOfCollision = 100
+timeSteps = 2*timeStepOfCollision - 1
+# Dimension (timeSteps)
+t = sp.linspace(0, sp.multiply(2,t_col), timeSteps)
 
-# outFile = open("LabValues","w+")
-sp.savez("LabValues", v1L, v2L, v1L_f, v2L_f, E_i, E_f, p_x_i, p_x_f, p_y_i, p_y_f)
+# Rework dimensions of initial position and velocity matrices during this step
 
+# Dimension (2(the # of SpatialDimensions), numTrials, timeSteps)
+Position1L_t_pre = sp.repeat(sp.transpose(InitialPosition1L)[:,:,sp.newaxis], timeSteps, axis=2)\
+               + sp.multiply( sp.repeat(sp.transpose(v1L)[:, :, sp.newaxis], timeSteps, axis=2), sp.transpose(t) )
+Position2L_t_pre = sp.repeat(sp.transpose(InitialPosition2L)[:,:,sp.newaxis], timeSteps, axis=2) \
+               + sp.multiply( sp.repeat(sp.transpose(v2L)[:, :, sp.newaxis], timeSteps, axis=2), sp.transpose(t) )
+Position1L_t_post = sp.repeat(Position1L_t_pre[:,:,timeStepOfCollision][:,:,sp.newaxis], timeSteps, axis=2) \
+               + sp.multiply( sp.repeat(sp.transpose(v1L_f)[:, :, sp.newaxis], timeSteps, axis=2), sp.transpose(t) )
+Position2L_t_post = sp.repeat(Position2L_t_pre[:,:,timeStepOfCollision][:,:,sp.newaxis], timeSteps, axis=2) \
+               + sp.multiply( sp.repeat(sp.transpose(v2L_f)[:, :, sp.newaxis], timeSteps, axis=2), sp.transpose(t) )
+Position1L_t = sp.concatenate((Position1L_t_pre[:,:,:timeStepOfCollision],Position1L_t_post[:,:,timeStepOfCollision:]),2)
+Position2L_t = sp.concatenate((Position2L_t_pre[:,:,:timeStepOfCollision],Position2L_t_post[:,:,timeStepOfCollision:]),2)
+Velocity1L_t_pre = sp.repeat(sp.transpose(v1L)[:, :, sp.newaxis], timeSteps, axis=2)
+Velocity2L_t_pre = sp.repeat(sp.transpose(v2L)[:, :, sp.newaxis], timeSteps, axis=2)
+Velocity1L_t_post = sp.repeat(sp.transpose(v1L_f)[:, :, sp.newaxis], timeSteps, axis=2)
+Velocity2L_t_post = sp.repeat(sp.transpose(v2L_f)[:, :, sp.newaxis], timeSteps, axis=2)
+Velocity1L_t = sp.concatenate((Velocity1L_t_pre[:,:,:timeStepOfCollision],Velocity1L_t_pre[:,:,timeStepOfCollision:]),2)
+Velocity2L_t = sp.concatenate((Velocity2L_t_pre[:,:,:timeStepOfCollision],Velocity2L_t_pre[:,:,timeStepOfCollision:]),2)
 
-# print(m1_Arr)
+# Save arrays to file
+print("Saving: ")
+print(str(time.time() - start_time)+" seconds")
+sp.savez("LabValues", v1L, v2L, v1L_f, v2L_f, E_i, E_f, p_x_i, p_x_f, p_y_i, p_y_f,
+         t, Position1L_t, Position2L_t, Velocity1L_t, Velocity2L_t)
+print("Time to run: ")
+print(str(time.time() - start_time)+" seconds")
